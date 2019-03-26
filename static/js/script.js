4e308634bc17962c133d64c7f68d9fd728b8afbc
juancaio <juancaiops@gmail.com>
2019-03-26 10:06:01 -0300

    Adicionando script para adicionar e remover categoria - teste

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-//Add new category in box
+/*Add new category in box
 $("#add-category").click(function(){
     $("#category").append(" <div class='col s5'> <input type='text' id='nameCategory' name='nameCategory' placeholder='Nome da Categoria'>" +    
     '</div>'+
@@ -13,11 +13,84 @@ $("#add-category").click(function(){
     '</div>'+
     '<div class="col s2">'+
     "</div>");
-  });
+  });*/
 
-  //Dropdown in menu
+  //Dropdown in menu using Materialize JS
 
   $(document).ready(function(){
     $(".dropdown-trigger").dropdown({hover:false});
   });
+
+
+//Add and remove new category in box 'text two'
+
+//Total máximo de campos que você permitirá criar em seu site:
+var totalCampos = 10;
+
+//Variaveis de controle
+var iLoop = 1;
+var iCount = 0;
+var linhaAtual;
+
+function AddCampos() {    
+    var hidden1 = document.getElementById("hidden1");
+    var hidden2 = document.getElementById("hidden2");
+
+    //Executar apenas se houver possibilidade de inserção de novos campos:
+    if (iCount < totalCampos) {
+        //Limpar hidden1, para atualizar a lista dos campos que ainda estão vazios:
+        hidden2.value = "";
+
+        //Atualizando a lista dos campos que estão ocultos.
+        //Essa lista ficará armazenada temporiariamente em hidden2;
+        for (iLoop = 1; iLoop <= totalCampos; iLoop++) {
+            if (document.getElementById("linha"+iLoop).style.display == "none") {
+                    if (hidden2.value == "") {
+                            hidden2.value = "linha"+iLoop;
+                    }else{
+                            hidden2.value += ",linha"+iLoop;
+                    }
+            }
+        }
+        //Quebrando a lista que foi armazenada em hidden2 em array:
+        linhasOcultas = hidden2.value.split(",");
+        if (linhasOcultas.length > 0) {
+            //Tornar visível o primeiro elemento de linhasOcultas:
+            document.getElementById(linhasOcultas[0]).style.display = "block"; iCount++;
+            
+            //Acrescentando o índice zero a hidden1:
+            if (hidden1.value == "") {
+                    hidden1.value = linhasOcultas[0];
+            }else{
+                    hidden1.value += ","+linhasOcultas[0];
+            }
+            
+        }
+    }
+}
+
+function RemoverCampos(id) {
+    //Criando ponteiro para hidden1:        
+    var hidden1 = document.getElementById("hidden1");
+    //Pegar o valor do campo que será excluído:
+    var campoValor = document.getElementById("arq"+id).value;
+    //Se o campo não tiver nenhum valor, atribuir a string: vazio:
+    if (campoValor == "") {
+            campoValor = "vazio";
+    }
+
+    if(confirm("O campo que contém o valor:\n» "+campoValor+"\nserá excluído!\n\nDeseja prosseguir?")){
+            document.getElementById("linha"+id).style.display = "none"; iCount--;
+            
+            //Removendo o valor de hidden1:
+            if (hidden1.value.indexOf(",linha"+id) != -1) {
+                    hidden1.value = hidden1.value.replace(",linha"+id,"");
+            }else if (hidden1.value.indexOf("linha"+id+",") == 0) {
+                    hidden1.value = hidden1.value.replace("linha"+id+",","");
+            }else{
+                    hidden1.value = "";
+            }
+    }
+}    
+
  
