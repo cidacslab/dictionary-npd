c2fd4901b43a69abd84bffc620cc9540e25b665a
Luan Menezes <luancmenezes@localhost.localdomain>
2019-04-08 10:50:03 -0300

    mudança na preparaçao da string para insert no banco

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, render_template
 import os
 import json
+from flask_pymongo import PyMongo
 
 project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, 'templates/')
 
 app = Flask(__name__, template_folder=template_path)
+app.config['MONGO_DBNAME'] = 'dictionaryDB'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/dictionaryDB"
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
@@ -13,8 +18,18 @@ def hello_world():
 
 @app.route('/teste', methods=['POST']) 
 def teste():
-    values = str(request.form.get('result'))
-    print (values.replace('-,','-'))
-    print (values.replace('-,','-').split('-'))
-    return values
+        nameDictionary = str(request.form.get('nameDictionary'))
+        variables = str(request.form.get('result'))
+    
+        variables  = variables.replace("'",'"').replace('-,','-').split('-')
+
+        for var in variables:
+                if var is not '':
+                        try:
+                                var = var.replace(",}","}")
+                                var = json.loads(var)
+                        except:
+                                return "erro"
+                        mongo.db[nameDictionary].insert(var)    
+        return str(variables),nameDictionary
 
