3dbd5aaf54cf6e671d2c123b5ee4c5a9d180e961
Luan Menezes <luancmenezes@localhost.localdomain>
2019-04-18 16:21:26 -0300

    PyMongo mudança

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import os
+import sys
 import json
 import pandas as pd
 from pymongo import MongoClient
@@ -8,13 +9,25 @@ project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, 'templates/')
 
 app = Flask(__name__, template_folder=template_path)
-app.config['MONGO_DBNAME'] = 'dictionaryDB'
-app.config["MONGO_URI"] = "mongodb://localhost:27017/dictionaryDB"
+# app.config['MONGO_DBNAME'] = 'dictionaryDB'
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/dictionaryDB"
 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client.dictionaryDB
+def pymongo_python_sys():
+        if sys.version_info.major == 2:
+                app.config['MONGO_DBNAME'] = 'dictionaryDB'
+                app.config["MONGO_URI"] = "mongodb://localhost:27017/dictionaryDB"
+                mongo = PyMongo(app)
+        else:
+                client = MongoClient('mongodb://localhost:27017/')
+                mongo = client.dictionaryDB
+        return mongo
 
+# mongo = pymongo_python_sys()
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 @app.route('/teste', methods=['POST']) 
@@ -31,18 +44,18 @@ def teste():
                                 var = json.loads(var)
                         except:
                                 return "erro"
-                        mongo.db[nameDictionary].insert(var)    
-        return str(variables),nameDictionary
-        
-def pandas_to_csv():
-        client = MongoClient('localhost', 27017)
-        db = client.dictionaryDB
-        collection = db.hans
+                        collection = db[nameDictionary]
+                        collection.insert(var)    
+        return render_template('index.html')
+# def pandas_to_csv():
+#         client = MongoClient('localhost', 27017)
+#         db = client.dictionaryDB
+#         collection = db.hans
 
         df = pd.DataFrame(list(collection.find()))
         df[['variable','categories_std','type'].to_csv('/dictionary/name.csv',index=False)
            
 
 @app.route('/dictionary')
-def dictionary():
+def dictionary():0e72578
         return render_template('dictionary.html')
